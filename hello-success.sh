3328859a504116e9b95edb1c922c6fb8f78e75a2
esumana <ekongsimpson@gmail.com>
2022-12-03 23:53:52 +0100

    1st - updated

--- hello-success.sh
+++ hello-success.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "SUPER SUCCESS!!!!!!!!!!"
+echo "SUPER SUCCESS!!!!!!!!!!     "
 echo "2nd SUPER SUCCESS!!!!!!!!!! "
 echo "3rd SUPER SUCCESS!!!!!!!!!! "
 echo "4th SUPER SUCCESS!!!!!!!!!! "
